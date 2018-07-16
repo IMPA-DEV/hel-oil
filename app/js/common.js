862d5822bb7b90125f626c7b556e7a8b5627fa3d
Kualkua K <i.kualkua@gmail.com>
2018-07-16 19:51:08 +0300

    добавил локал сторыж (начал)

--- app/js/common.js
+++ app/js/common.js
@@ -40,6 +40,19 @@ $(document).ready(function(){
         ]
     });
 
+    // news-slider
+    var windowW = $(window).width();
+    if(windowW < 767){
+        $('.news-slider__row').slick({
+            speed: 500,
+            infinite: true,
+            slidesToShow: 1,
+            dots: true,
+            arrows: false,
+            adaptiveHeight: true,
+        });
+    }
+
     /* nav */
     $('.nav-btn').click(function () {
         $('.header__nav').fadeToggle();
@@ -78,7 +91,7 @@ $(document).ready(function(){
     $('.header__nav').css('opacity', '1')
   });
 
-  // card
+  // card - tabs
 
   var cardBtn = $('.card-pack__images-link');
   var cardImg = $('.card-pack__images-img');
@@ -119,18 +132,73 @@ $(document).ready(function(){
     });
   }
 
-  // news-slider
-  var windowW = $(window).width();
-  if(windowW < 767){
-    $('.news-slider__row').slick({
-      speed: 500,
-      infinite: true,
-      slidesToShow: 1,
-      dots: true,
-      arrows: false,
-      adaptiveHeight: true,
+
+  /* form next */
+    $('.next-form-2').on('click', function () {
+        $('.cart-form').hide();
+        $('.form-2').show();
+        $('.links__item-opa').removeClass('active');
+        $('.opa-2').addClass('active')
+    })
+
+    $('.next-form-3').on('click', function () {
+        $('.cart-form').hide();
+        $('.form-3').show();
+        $('.links__item-opa').removeClass('active');
+        $('.opa-3').addClass('active')
+    })
+
+
+    /* form prev */
+    $('.prev-form-2').on('click', function () {
+        $('.cart-form').hide();
+        $('.form-2').show();
+        $('.links__item-opa').removeClass('active');
+        $('.opa-2').addClass('active')
+    })
+
+    $('.prev-form-1').on('click', function () {
+        $('.cart-form').hide();
+        $('.form-1').show();
+        $('.links__item-opa').removeClass('active');
+        $('.opa-1').addClass('active')
+    })
+
+    /* card - form */
+    $('.card-pack__images-link').on('click', function (){
+        $('.card-pack__images-link').removeClass('cart-sel');
+        $(this).addClass('cart-sel')
     });
-  }
+
+
+    var addCart = document.getElementById('addCart');
+
+    $(addCart).on('click', function (e) {
+        var cardActive = document.querySelector('.cart-sel');
+
+        var dataQuantity = $('#select-quantity').val();
+        var dataName = $(cardActive).data('name');
+        var dataSum = $(cardActive).data('sum');
+        var dataShipping = $(cardActive).data('shipping');
+        var dataImage = $(cardActive).data('image');
+
+        localStorage.setItem('image', dataImage);
+        localStorage.setItem('name', dataName);
+        localStorage.setItem('sum', dataSum);
+        localStorage.setItem('shipping', dataShipping);
+        localStorage.setItem('quantity', dataQuantity);
+    });
+
+    var packageImg = localStorage.setItem('image');
+    var packageSum = localStorage.setItem('sum');
+    var packageName = localStorage.setItem('name');
+    var packageTotal = localStorage.setItem('shipping', dataShipping)*localStorage.setItem('quantity', dataQuantity);
+
+    $('#package-name').innerText(packageName)
+    $('#package-sum').innerText(packageSum)
+
+
+
 });
 
 
